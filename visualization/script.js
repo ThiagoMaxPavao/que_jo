7d971de49d129498bfda37736b8be62a673d2388
Thiago <thipavao44@gmail.com>
2024-01-06 12:10:17 -0300

    first vertex drag working

--- visualization/script.js
+++ visualization/script.js
@@ -1,28 +1,86 @@
+var app;
+var dragTarget = null;
 
-const app = new PIXI.Application({
-    background: '#1099bb',
-    resizeTo: window,
-});
+function onDragMove(event) {
+    if (!dragTarget) return;
+    dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+}
 
-document.body.appendChild(app.view);
+function onDragStart() {
+    this.alpha = 0.5;
+    dragTarget = this;
+    app.stage.on('pointermove', onDragMove);
+}
 
-// create a new Sprite from an image path
-const bunny = PIXI.Sprite.from('https://pixijs.com/assets/bunny.png');
+function onDragEnd() {
+    if (!dragTarget) return;
+    app.stage.off('pointermove', onDragMove);
+    dragTarget.alpha = 1;
+    dragTarget = null;
+}
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+class vertice {
+    constructor(label, x, y) {
+        
+        let container = new PIXI.Container();
+        container.eventMode = 'static';
+        container.cursor = 'pointer';
+        container.x = x;
+        container.y = y;
 
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+        let circleElement = new PIXI.Graphics();
+        circleElement.lineStyle(0);
+        circleElement.beginFill(0xFFFF0B, 0.5);
+        circleElement.drawCircle(0, 0, 60);
+        circleElement.endFill();
+        container.addChild(circleElement);
+        
+        const labelElement = new PIXI.Text(label);
+        labelElement.anchor.set(0.5);
+        container.addChild(labelElement);
 
-app.stage.addChild(bunny);
+        container.on('pointerdown', onDragStart);
 
-// Listen for animate update
-app.ticker.add((delta) =>
-{
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+        app.stage.addChild(container);
+
+        this.label = label;
+    }
+}
+
+class DiGraph {
+    constructor() {
+        this.vertices = [];
+        this.edges = [];
+    }
+
+
+}
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+
+    const firstSyllable = event.target.querySelector('#first-syllable').value;
+    
+    const form = document.querySelector('form');
+    form.remove();
+
+    startApp(firstSyllable);
+}
+
+function startApp(firstSyllable) {
+
+    app = new PIXI.Application({
+        background: '#1099bb',
+        resizeTo: window,
+    });
+
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+    app.stage.on('pointerup', onDragEnd);
+    app.stage.on('pointerupoutside', onDragEnd);
+
+    document.body.appendChild(app.view);
+
+    new vertice(firstSyllable, app.screen.width / 2, app.screen.height / 2);
+}
+
